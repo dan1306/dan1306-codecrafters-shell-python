1f68d0f4be816319d618510d0087e180acc4ee5c
dan1306 <81488150+dan1306@users.noreply.github.com>
2024-05-26 09:36:16 -0400

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("$ ")
+    # print("$ ")
 
     # Uncomment this block to pass the first stage
     # sys.stdout.write("$ ")
