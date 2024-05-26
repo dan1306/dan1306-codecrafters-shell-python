2f5f78097afe8eba7a419df596a628ea2e0edd83
dan1306 <81488150+dan1306@users.noreply.github.com>
2024-05-26 12:02:53 -0400

    pass 6th  stage

--- app/main.py
+++ app/main.py
@@ -30,16 +30,24 @@ def main():
         user_Input = input()
 
         user_input_for_echo = split_with_spaces(user_Input)
-
-
-
-        if(user_Input == "exit 0"):
-            sys.exit(0)
-        elif(user_input_for_echo[0] == "echo"):
-            print_this_out = "".join(user_input_for_echo[1:])
-            print(print_this_out)
+        
+        if(user_input_for_echo[0] == "type"):
+            if(user_input_for_echo[2] == "echo"):
+                print("echo is a shell builtin")
+            elif(user_input_for_echo[2] == "exit"):
+                print("exit is a shell builtin")
+            elif(user_input_for_echo[2] == "type"):
+                print("type is a shell builtin")
+            elif(user_input_for_echo[2] == "nonexistent"):
+                print("type is a shell builtin")
         else:
-            print(f"{user_Input}: command not found")
+            if(user_Input == "exit 0"):
+                sys.exit(0)
+            elif(user_input_for_echo[0] == "echo"):
+                print_this_out = "".join(user_input_for_echo[1:])
+                print(print_this_out)
+            else:
+                print(f"{user_Input}: command not found")
 
 
 if __name__ == "__main__":
